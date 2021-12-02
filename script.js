e140fda4030c743ea59aa981a848ab3ddf6ce7c6
Higor Lara <higorv.lara@gmail.com>
2021-12-02 20:01:44 -0300

    portfolio finished

--- script.js
+++ script.js
@@ -16,4 +16,28 @@ function changePosition(element) {
       behavior: "smooth",
     });
   }
+  handleHeader();
+}
+
+function handleHeader() {
+  var line = document.querySelector(".line1").classList.contains("first_line");
+  var boolean = document
+    .querySelector(".just_buttons_container")
+    .classList.contains("active");
+  if (boolean) {
+    document
+      .querySelector(".just_buttons_container")
+      .classList.remove("active");
+  } else {
+    document.querySelector(".just_buttons_container").classList.add("active");
+  }
+  if (line) {
+    document.querySelector(".line1").classList.remove("first_line");
+    document.querySelector(".line2").classList.remove("second_line");
+    document.querySelector(".line3").classList.remove("third_line");
+  } else {
+    document.querySelector(".line1").classList.add("first_line");
+    document.querySelector(".line2").classList.add("second_line");
+    document.querySelector(".line3").classList.add("third_line");
+  }
 }
